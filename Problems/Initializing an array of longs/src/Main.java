d25895c3212a4066d7c0088b3eaf8d8f4de385ed
Владелец <leonid82@yandex.ru>
2020-10-07 13:58:55 +0300

    Решение задач

---
+++ Problems/Initializing an array of longs/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        long[] longNumbers = {1000000001L, 100000000002L, 10000000000L};
+
+        System.out.println(Arrays.toString(longNumbers));
+    }
+}
