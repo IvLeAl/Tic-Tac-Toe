fadab94816abe40812d39c0aee15523fd8410ac6
Владелец <leonid82@yandex.ru>
2020-10-07 14:34:35 +0300

    Решение задач

--- Problems/Initializing an array of integers/src/Main.java
+++ Problems/Initializing an array of integers/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         int[] numbers; // initialize the array
+        numbers = new int[] {12,17, 8, 101, 33};
 
         System.out.println(Arrays.toString(numbers));
     }
--- Problems/Initializing an array of longs/src/Main.java
+++ Problems/Initializing an array of longs/src/Main.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String args[]) {
+    public static void main(String [] args) {
 
-        long[] longNumbers = {1000000001L, 100000000002L, 10000000000L};
+        long[] longNumbers = {100000000001L, 100000000002L, 100000000003L};
 
         System.out.println(Arrays.toString(longNumbers));
     }
