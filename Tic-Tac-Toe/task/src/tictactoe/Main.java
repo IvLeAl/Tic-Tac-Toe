f87452463285d92f46c4fbfd9a380619b990e7c1
Владелец <leonid82@yandex.ru>
2020-10-08 14:03:00 +0300

    Решаем задачи

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,10 +1,22 @@
 package tictactoe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("XOX");
-        System.out.println("OXO");
-        System.out.println("XOX");
+        Scanner scanner = new Scanner(System.in);
+
+        String  inputValue;
+        String enter = "Enter cells: ";
+
+        System.out.println(enter + (inputValue = scanner.nextLine()));
+
+        System.out.println("---------");
+        System.out.println("| " + inputValue.charAt(0) + " " + inputValue.charAt(1) + " " + inputValue.charAt(2) + " |");
+        System.out.println("| " + inputValue.charAt(3) + " " + inputValue.charAt(4) + " " + inputValue.charAt(5) + " |");
+        System.out.println("| " + inputValue.charAt(6) + " " + inputValue.charAt(7) + " " + inputValue.charAt(8) + " |");
+        System.out.println("---------");
+
     }
 }
