8fffad1ff9848af92de6770ce4e6325b7308031c
Владелец <leonid82@yandex.ru>
2020-10-13 19:00:33 +0300

    Задачи

--- Problems/Cyclically shifting elements/src/Main.java
+++ Problems/Cyclically shifting elements/src/Main.java
@@ -4,5 +4,16 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        int b = scanner.nextInt();
+        int[] a = new int[b];
+
+
+        for ( int i= 0; i < b; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int [] dst = new int[b];
+        System.arraycopy(a, 1, dst , 0, 4);
+        dst [0] = a[i]
+
     }
 }
