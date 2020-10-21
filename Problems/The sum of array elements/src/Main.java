a2298c9feb7368815b024e519ac509000e0caf8e
Владелец <leonid82@yandex.ru>
2020-10-21 11:48:57 +0300

    Задачи

--- Problems/The sum of array elements/src/Main.java
+++ Problems/The sum of array elements/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int[] arr = new int[a];
+
+        for( int i = 0; i < arr.length; i++){
+            arr[i] = scanner.nextInt();
+        }
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.print(sum);
     }
 }
+
+// Второй вариант
+
+/*public static void main(String[] args) {
+        // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int[] arr = new int[a];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int sum = Arrays.stream(arr).sum();
+        System.out.print(sum);
+    }
+}*/
