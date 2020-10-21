af390dbbdeb0cfdee53649093ada7892c8afac8d
Владелец <leonid82@yandex.ru>
2020-10-21 11:38:22 +0300

    Задачи

--- Problems/Count how many times a number occurs/src/Main.java
+++ Problems/Count how many times a number occurs/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int[] arr = new int[a];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        int b = scanner.nextInt();
+        int s = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == b) {
+                s++;
+            }
+        }
+        System.out.print(s);
     }
 }
